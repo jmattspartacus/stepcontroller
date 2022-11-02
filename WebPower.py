b9276bf2ff42f0a3bcc5a1def51b5bf094153964
James Christie <mattacusspartacus@gmail.com>
2022-11-02 19:44:57 -0400

    Fix: WebPower no longer errors for missing script

--- WebPower.py
+++ WebPower.py
@@ -1,12 +1,16 @@
-import subprocess 
+import subprocess, os
 import Logger
 
 class WebPower:
     def __init__(self, logger: Logger.Logger, index: int):
         self.logger = logger
         self.index=index
+    
 
     def PowerOn(self):
+        if not os.path.exists("./lnrelay-eth.sh"):
+            print("Failed to find the webpower script for toggling power!")
+            return    
         retval=subprocess.check_output(["./lnrelay-eth.sh",str(self.index),"ON"]).decode()
         if (int(retval) != 0):
             print("Warning! Error in ./lnrelay-eth.sh")
@@ -14,6 +18,9 @@ class WebPower:
         return True
 
     def PowerOff(self):
+        if not os.path.exists("./lnrelay-eth.sh"):
+            print("Failed to find the webpower script for toggling power!")
+            return    
         retval=subprocess.check_output(["./lnrelay-eth.sh",str(self.index),"OFF"]).decode()
         if (int(retval) != 0):
             print("Warning! Error in ./lnrelay-eth.sh")
@@ -21,6 +28,9 @@ class WebPower:
         return True
     
     def CheckStatus(self):
+        if not os.path.exists("./lnrelay-check.sh"):
+            print("Failed to find the webpower script for checking status!")
+            return    
         retval=subprocess.check_output(["./lnrelay-check.sh",str(self.index)]).decode()
         retval=retval.split("\n")
         if (int(retval[1]) != 0):
